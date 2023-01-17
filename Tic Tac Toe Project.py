54ee8512874618491e6d3f0dd23c2c58a9248ee2
BRYANXII <103326259+BRYANXII@users.noreply.github.com>
2023-01-17 07:54:25 -0600

    test 2

---
+++ Tic Tac Toe Project.py
@@ -0,0 +1,58 @@
+#Tic Tac Toe Project\
+print("Welcome to Tic Tac Toe")
+
+def display_board(board):
+    test_board= [['']*3,
+['']*3,
+['']*3]
+    print(test_board[0])
+    print(test_board[1])
+    print(test_board[2])
+# test_board = ['#','X','O','X','O','X','O','X','O','X']
+display_board(board)
+
+def player_input():
+    pass
+
+player_input()
+
+def place_marker(board, marker, postition):
+    pass
+
+place_marker(test_board,'$',8)
+display_board(test_board)
+
+def win_check(board,mark):
+    pass
+
+win_check(test_board,'X')
+
+import random
+
+def choose_first():
+    pass
+
+def space_check(board, poistion):
+    pass
+
+def full_board_check(board):
+    pass
+def replay():
+    pass
+
+print('Welcome to Tic Tac Toe!')
+
+#while True:
+    # Set the game up here
+    #pass
+
+    #while game_on:
+        #Player 1 Turn
+        
+        
+        # Player2's turn.
+            
+            #pass
+
+    #if not replay():
+        #break
