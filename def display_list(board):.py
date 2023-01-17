6bb4bae688029422fef306010f70464ed2bfa236
BRYANXII <103326259+BRYANXII@users.noreply.github.com>
2023-01-17 07:54:13 -0600

    test 1

---
+++ def display_list(board):.py
@@ -0,0 +1,24 @@
+def display_list(board):
+   test_board= [['']*3,
+['']*3,
+['']*3]
+    print(test_board[0])
+    print(test_board[1])
+    print(test_board[2])
+   # top_row = ['']*3
+   # mid_row = ['']*3
+   # bottom_row = ['']*3
+   # print(top_row)
+   # print(mid_row)
+   # print(bottom_row)
+
+display_list(board)
+
+def player_input():
+    user1_input = input('Select either X or O to begin: ') #append? 
+    input_options = ['X','O']
+    while user1_input not in input_options:
+            print('Please choose either X or O to begin')
+    if user1_input == input_options[0] or input_options[1]:
+            user1_input.apppend()
+player_input()
