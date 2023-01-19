acbd2127b3cf32170fb9919cce4a125694b32c02
BRYANXII <103326259+BRYANXII@users.noreply.github.com>
2023-01-18 20:40:50 -0600

    gameRunning func teset

---
+++ Tic Tac Toe Proj.py
@@ -0,0 +1,29 @@
+board = ['-','-','-',
+'-','-','-',
+'-','-','-']
+currentPlayer = 'X'
+winner = None
+gameRunning = True
+
+# print the game board 
+
+def display_list(board):
+    print(board[0]+ '|' +  board[1] + '|' + board[2])
+    print(board[3]+ '|' +  board[4] + '|' + board[5])
+    print(board[6]+ '|' + board[7] + '|' + board[8])
+
+display_list(board)
+
+# take player input 
+def playerInput(board):
+    inp = int(input('Enter a number 1-9: '))
+    if inp >= 1 and inp <= 9 and board[inp-1] == '-':
+        board[inp-1] = currentPlayer
+    elif inp not in range (1,10):
+        print('Please select a valid input of 1-9!')
+    else: 
+        print('Oops spot already taken!')
+
+while gameRunning:
+    display_list(board)
+    playerInput(board)
--- def display_list(board):.py
+++ def display_list(board):.py
@@ -1,10 +1,13 @@
-def display_list():
-   test_board= [['']*3,
-['']*3,
-['']*3]
-   print(test_board[0])
-   print(test_board[1])
-   print(test_board[2])
+test_board = [['-']*3,
+['-']*3,
+['-']*3]
+currentPlayer = 'X'
+winner = None
+gameRunning = True
+def display_list(test_board):
+   print(test_board[0][0] + '|' + test_board[0][1] + '|' + test_board[0][2])
+   print(test_board[1][0] + '|' + test_board[1][1] + '|' + test_board[1][2])
+   print(test_board[2][0] + '|' + test_board[2][1] + '|' + test_board[2][2])
    # top_row = ['']*3
    # mid_row = ['']*3
    # bottom_row = ['']*3
@@ -12,15 +15,37 @@ def display_list():
    # print(mid_row)
    # print(bottom_row)
 
-display_list()
+display_list(test_board)
+
+def playerInput(test_board):
+    inp = int(input('Enter a number 1-9: '))
+    if inp >= 1 and inp <= 9 and board[inp-1] == '-':
+        board[inp-1] = currentPlayer
+    # elif inp not >= 1 or inp not <=9:
+      # print('Please enter a number 1-9!')
+    elif inp not in range (1,10):
+      print('Please enter a number 1-9!')
+    else:
+        print('Oops spot already taken!')
+
+while gameRunning:
+   display_list(test_board)
+   playerInput(test_board)
+
+
 
 def player_input():
     user1_input = input('Select either X or O to begin: ') #append? 
     input_options = ['X','O']
-    while user1_input not in input_options:
+    player_1 = []
+    if user1_input not in input_options:
             print('Please choose either X or O to begin')
-    if user1_input == input_options[0] or input_options[1]:
-            user1_input.apppend()
+    elif user1_input == input_options:
+            print('You are now {user1_input}, ready to begin?')
+            user1_input.append(player_1)
+      else:
+         print('Please choose either X or O to begin')
+   
 player_input()
 
 #def test_func():
